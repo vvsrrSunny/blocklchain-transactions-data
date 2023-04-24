04f0253c49588171f80e307c8c7b9716dcdee1ad
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-04-24 20:24:50 +0800

    updated to ant design

--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -1,4 +1,4 @@
-import { Typography, theme } from "antd";
+import { Col, Row, Typography, theme } from "antd";
 import { Header } from "antd/es/layout/layout";
 import React from "react";
 
@@ -7,15 +7,22 @@ const HeaderContent = () => {
     const {
         token: { colorBgContainer },
     } = theme.useToken();
-    
+
     return (
         <Header style={{ padding: 0, background: colorBgContainer }} >
-        <div className="flex h-full justify-center items-center">
-            <Typography.Title style={{ margin: "0" }}>
-                Blockchain Dashboard
-            </Typography.Title>
-        </div>
-    </Header>
+            {/* <div className="flex h-full justify-center items-center">
+                <Typography.Title style={{ margin: "0" }}>
+                    Blockchain Dashboard
+                </Typography.Title>
+            </div> */}
+            <Row justify="center" align="middle" className="h-full">
+                <Col>
+                    <Typography.Title style={{ margin: "0" }}>
+                        Blockchain Dashboard
+                    </Typography.Title>
+                </Col>
+            </Row>
+        </Header>
     );
 };
 
