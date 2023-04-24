8b865d3eb4b03cb239f482e39acb5b8bfd4a7c71
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-04-24 20:25:13 +0800

    removed the code

--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -10,11 +10,6 @@ const HeaderContent = () => {
 
     return (
         <Header style={{ padding: 0, background: colorBgContainer }} >
-            {/* <div className="flex h-full justify-center items-center">
-                <Typography.Title style={{ margin: "0" }}>
-                    Blockchain Dashboard
-                </Typography.Title>
-            </div> */}
             <Row justify="center" align="middle" className="h-full">
                 <Col>
                     <Typography.Title style={{ margin: "0" }}>
