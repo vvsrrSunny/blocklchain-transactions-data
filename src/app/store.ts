6e5d507346a584986371093a30ea1f380664ba2c
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2023-04-24 22:08:38 +0800

    removed all the example counter redux files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    //$$ set the store and add reducer
   },
 });
 
